a5f861768ff568bd44f7dc269e2e1a719d67e75b
Rob Crowther <rw.crowther@gmail.com>
2020-06-18 15:34:24 +0100

    delete managements command not use title attr

--- management/commands/image_delete.py
+++ management/commands/image_delete.py
@@ -9,32 +9,38 @@ class Command(BaseCommand):
 
 
     def add_arguments(self, parser):
-        parser.add_argument('image_titles', nargs='*', type=str)
+        #parser.add_argument('image_titles', nargs='*', type=str)
 
         parser.add_argument(
-            '--all',
+            '--bulk',
             action='store_true',
-            help='Delete all images',
+            help='Delete images by SQL (fast, but less robust)',
         )
 
         
     def handle(self, *args, **options):
-        if options['all']:
+        if options['bulk']:
             #! this going to work on reform relationship table? I hope so.
             r = Image.objects.all().delete()
             
             if (options['verbosity'] > 0):
                 print("{} image(s) deleted".format(r[0])) 
         else:
-            if (not(options['image_titles'])):
-                if (options['verbosity'] > 0):
-                   print('no args, very safe?')
-            else:
-                count = 0
-                for ititle in options['image_titles']:
-                    r = Image.objects.filter(title=ititle).delete()
-                    # count from return. May have failed!
-                    count += r[0]
+            # if (not(options['image_titles'])):
+                # if (options['verbosity'] > 0):
+                   # print('no args, very safe?')
+            # else:
+            count = 0
+            #for ititle in options['image_titles']:
+            #    r = Image.objects.filter(title=ititle).delete()
+                # count from return. May have failed!
+            #    count += r[0]
+            qs = Image.objects.all()
+            for im in qs:
+                r = im.delete()
+                
+                # count from return. May have failed!
+                count += r[0]
 
-                if (options['verbosity'] > 0):
-                    print("{} image(s) deleted".format(count)) 
+            if (options['verbosity'] > 0):
+                print("{} image(s) deleted".format(count)) 
